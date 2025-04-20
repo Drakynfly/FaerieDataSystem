548d77ae02f26e0b0b8140af2dc559b9e83bfecd
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2025-04-20 16:20:15 -0700

    make Mutability pin advanced by default

--- Source/FaerieItemData/Private/Libraries/FaerieItemDataLibrary.h
+++ Source/FaerieItemData/Private/Libraries/FaerieItemDataLibrary.h
@@ -28,10 +28,10 @@ public:
 	static bool Equal_ItemToken(const UFaerieItemToken* A, const UFaerieItemToken* B);
 
 	// Get the item instance this asset represents. By default, this will return the immutable asset if possible.
-	UFUNCTION(BlueprintPure, Category = "Faerie|ItemAsset")
+	UFUNCTION(BlueprintPure, Category = "Faerie|ItemAsset", meta = (AdvancedDisplay = 1))
 	static UFaerieItem* GetItemInstance(const UFaerieItemAsset* Asset, const EFaerieItemInstancingMutability Mutability);
 
-	UFUNCTION(BlueprintPure, Category = "Faerie|ItemInstance")
+	UFUNCTION(BlueprintPure, Category = "Faerie|ItemInstance", meta = (AdvancedDisplay = 1))
 	static UFaerieItem* NewItemInstance(const TArray<UFaerieItemToken*>& Tokens, EFaerieItemInstancingMutability Mutability);
 
 	// Try to get access to the editing API of a faerie item. This will only succeed if the item is Mutable, meaning
