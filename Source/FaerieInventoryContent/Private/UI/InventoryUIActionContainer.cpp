ac38b6f6d51a1a1e37f781e0a33885b24e1098e9
Drakynfly <47373221+Drakynfly@users.noreply.github.com>
2025-05-12 21:48:29 -0700

    add simple object to contain UIActions

---
+++ Source/FaerieInventoryContent/Private/UI/InventoryUIActionContainer.cpp
@@ -0,0 +1,40 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#include "UI/InventoryUIActionContainer.h"
+#include "UI/InventoryUIAction.h"
+
+bool UInventoryUIActionContainer::AddActionOfClass(const TSubclassOf<UInventoryUIAction2> Class)
+{
+	if (Class)
+	{
+		Actions.Add(NewObject<UInventoryUIAction2>(this, Class));
+	}
+	return true;
+}
+
+bool UInventoryUIActionContainer::AddActionInstance(UInventoryUIAction2* Action)
+{
+	if (Action)
+	{
+		Actions.Add(Action);
+	}
+	return true;
+}
+
+bool UInventoryUIActionContainer::RemoveActionOfClass(TSubclassOf<UInventoryUIAction2> Class)
+{
+	return !!Actions.RemoveAll(
+		[Class](const UInventoryUIAction2* Action)
+		{
+			return Action->GetClass() == Class;
+		});
+}
+
+bool UInventoryUIActionContainer::RemoveActionInstance(UInventoryUIAction2* Action)
+{
+	if (Action)
+	{
+		Actions.Remove(Action);
+	}
+	return true;
+}
---
+++ Source/FaerieInventoryContent/Public/UI/InventoryUIActionContainer.h
@@ -0,0 +1,34 @@
+﻿// Copyright Guy (Drakynfly) Lundvall. All Rights Reserved.
+
+#pragma once
+
+#include "UObject/Object.h"
+#include "InventoryUIActionContainer.generated.h"
+
+class UInventoryUIAction2;
+
+/**
+ * Contains an Array of UI actions.
+ */
+UCLASS(BlueprintType)
+class FAERIEINVENTORYCONTENT_API UInventoryUIActionContainer : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "UIActionContainer")
+	bool AddActionOfClass(TSubclassOf<UInventoryUIAction2> Class);
+
+	UFUNCTION(BlueprintCallable, Category = "UIActionContainer")
+	bool AddActionInstance(UInventoryUIAction2* Action);
+
+	UFUNCTION(BlueprintCallable, Category = "UIActionContainer")
+	bool RemoveActionOfClass(TSubclassOf<UInventoryUIAction2> Class);
+
+	UFUNCTION(BlueprintCallable, Category = "UIActionContainer")
+	bool RemoveActionInstance(UInventoryUIAction2* Action);
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "ActionContainer")
+	TArray<TObjectPtr<UInventoryUIAction2>> Actions;
+};
